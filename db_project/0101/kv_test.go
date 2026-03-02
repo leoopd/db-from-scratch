fe9de2625790e02aa309791b8a1548aac7161ad4
leo <leo.opdenplatz@gmail.com>
2026-03-02 15:31:24 +0100

    optimized unit tests > table tests for multiple case scenarios

--- db_project/0101/kv_test.go
+++ db_project/0101/kv_test.go
@@ -28,37 +28,51 @@ func TestGet(t *testing.T) {
 	kv := KV{}
 	kv.Open()
 
-	inputKey := []byte("known")
-	inputVal := []byte("123")
-	kv.mem[string(inputKey)] = inputVal
-	val, ok, err := kv.Get(inputKey)
-
-	if string(val) != string(inputVal) {
-		t.Errorf("wanted: val = %v; got: val = %v", inputVal, val)
+	testCases := []struct {
+		key         []byte
+		val         []byte
+		kv          KV
+		errExpected bool
+	}{
+		{
+			key: []byte("known"),
+			val: []byte("123"),
+			kv: func(kv KV) KV {
+				kv.mem[string("known")] = []byte("123")
+				return kv
+			}(kv),
+			errExpected: false,
+		},
+		{
+			key: []byte("unknown"),
+			kv: func(kv KV) KV {
+				return kv
+			}(kv),
+			errExpected: true,
+		},
 	}
 
-	if !ok {
-		t.Errorf("wanted: ok = true; got: ok = false")
-	}
-
-	if err != nil {
-		t.Errorf("wanted: err = nil; got: err = %v", err)
-	}
-
-	inputKey = []byte("unknown")
-	val, ok, err = kv.Get(inputKey)
-
-	if val != nil {
-		t.Errorf("wanted: val = nil; got val = %v", val)
-	}
-
-	if ok {
-		t.Errorf("wanted: ok = false; got: ok = true")
-	}
-
-	if err == nil {
-		fmt.Println(val == nil)
-		t.Errorf("wanted: err = not found; got: err = %v", err)
+	for _, tc := range testCases {
+		val, ok, err := kv.Get(tc.key)
+		if tc.errExpected {
+			if err == nil {
+				fmt.Println(val == nil)
+				t.Errorf("[expected to err] err = %v, key = %s", err, string(tc.key))
+			}
+			if ok {
+				t.Errorf("[expected to err] ok = true, key = %s", string(tc.key))
+			}
+		} else {
+			if string(val) != string(tc.val) {
+				t.Errorf("wanted: val = %v; got: val = %v", tc.val, val)
+			}
+			if !ok {
+				t.Errorf("wanted: ok = true; got: ok = false")
+			}
+			if err != nil {
+				t.Errorf("unexpected err = %v", err)
+			}
+		}
 	}
 }
 
@@ -82,29 +96,44 @@ func TestDel(t *testing.T) {
 	kv := KV{}
 	kv.Open()
 
-	inputKey := []byte("known")
-	inputVal := []byte("123")
-	kv.mem[string(inputKey)] = inputVal
-	ok, err := kv.Del(inputKey)
-	if err != nil {
-		t.Errorf("err not nil %v", err)
+	testCases := []struct {
+		key         []byte
+		kv          KV
+		errExpected bool
+	}{
+		{
+			key: []byte("known"),
+			kv: func(kv KV) KV {
+				kv.mem[string("known")] = []byte("123")
+				return kv
+			}(kv),
+			errExpected: false,
+		},
+		{
+			kv: func(kv KV) KV {
+				return kv
+			}(kv),
+			errExpected: true,
+		},
 	}
 
-	if !ok {
-		t.Errorf("wanted: ok = false; got: ok = true")
-	}
+	for _, tc := range testCases {
+		deleted, err := tc.kv.Del(tc.key)
+		if tc.errExpected {
+			if deleted {
+				t.Errorf("[expected to err] deleted = true. key: %s", string(tc.key))
+			}
+			if err == nil {
+				t.Errorf("[expected to err] err = nil. key: %s", string(tc.key))
+			}
+		} else {
+			if err != nil {
+				t.Errorf("err not nil %v. key: %s", err, string(tc.key))
+			}
 
-	if err != nil {
-		t.Errorf("wanted: err = nil; got: err = %v", err)
-	}
-
-	inputKey = []byte("unknown")
-	ok, err = kv.Del(inputKey)
-	if ok {
-		t.Errorf("wanted: ok = false; got: ok = true")
-	}
-
-	if err == nil {
-		t.Errorf("wanted: err = nil; got: err = %v", err)
+			if !deleted {
+				t.Errorf("wanted: deleted = false; got: deleted = true. key: %s", string(tc.key))
+			}
+		}
 	}
 }
