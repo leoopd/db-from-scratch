99db192958bdaebab6c0f42b3ca6af5d1290315a
leo <leo.opdenplatz@gmail.com>
2026-03-02 15:51:07 +0100

    updated unit test to match change in requirements for entries

--- db_project/0102/entries_test.go
+++ db_project/0102/entries_test.go
@@ -8,13 +8,24 @@ import (
 
 func TestDecode(t *testing.T) {
 	testCases := []struct {
-		entry Entry
+		entry   Entry
+		deleted bool
 	}{
 		{
 			entry: Entry{
-				key: []byte("thisIsSomeKey"),
-				val: []byte("thisIsSomeValue"),
+				key:     []byte("thisIsSomeKey"),
+				val:     []byte("thisIsSomeValue"),
+				deleted: false,
 			},
+			deleted: false,
+		},
+		{
+			entry: Entry{
+				key:     []byte("thisIsSomeKey"),
+				val:     []byte("thisIsSomeValue"),
+				deleted: true,
+			},
+			deleted: true,
 		},
 	}
 
@@ -24,6 +35,11 @@ func TestDecode(t *testing.T) {
 
 		out := Entry{}
 		err := out.Decode(b)
+		if tc.deleted {
+			if !out.deleted {
+				t.Errorf("want: deleted = true; got: deleted = false. err: %v, key: %s", err, string(tc.entry.key))
+			}
+		}
 		if err != nil {
 			t.Errorf("encountered err: %v", err)
 		}
@@ -32,6 +48,10 @@ func TestDecode(t *testing.T) {
 			t.Errorf("keys don't match. want: %s, got: %s", string(tc.entry.key), string(out.key))
 		}
 		fmt.Println("Value:", string(out.val))
+		if string(out.val) != string(tc.entry.val) {
+			t.Errorf("values don't match. want: %s, got: %s", string(tc.entry.val), string(out.val))
+		}
+		fmt.Println("Deleted:", out.deleted)
 		if string(out.val) != string(tc.entry.val) {
 			t.Errorf("values don't match. want: %s, got: %s", string(tc.entry.val), string(out.val))
 		}
